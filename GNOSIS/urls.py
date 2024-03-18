269923f03a432a26ed73ab907479e77b260bce8a
MidHunterX <midhunterx@gmail.com>
2024-03-18 16:42:30 +0530

    🌐 Finalized URLs

--- GNOSIS/urls.py
+++ GNOSIS/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+# For Media Dir variables
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("gnosis_app.urls")),
     path('admin/', admin.site.urls),
 ]
+
+# For Media
+urlpatterns = urlpatterns + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
---
+++ gnosis_app/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from . import responses
+
+# Namespace
+app_name = 'gnosis'
+
+# URL Patterns
+urlpatterns = [
+    path("test/", responses.test),
+    path("", responses.greetings, name="henlo"),
+
+    path('home/',responses.ques_list,name = 'ques_list'),
+    path('ques/<int:id>/',responses.ques_detail,name='ques_detail'),
+    path('login/',responses.user_login,name = 'user_login'),
+    path('logout/',responses.user_logout ,name = 'user_logout'),
+    path('register/',responses.register,name = 'register'),
+    path('edit_profile/',responses.edit_profile , name = 'edit_profile'),
+    path('ask_question/',responses.ask_question,name = 'ask_question'),
+    path('update_ques/<int:id>/',responses.update_ques ,name = 'update_ques'),
+    path('delete_ques/<int:id>/', responses.delete_ques, name='delete_ques'),
+    path('likes/',responses.ques_likes,name ='ques_likes'),
+    path('comment_reply/<int:id>',responses.comment_reply,name = 'comment_reply'),
+    path('profilepage/<str:username>/',responses.profilepage,name='profilepage'),
+    path('delete_comment/<int:id>',responses.delete_comment,name='delete_comment'),
+    path('delete_reply/<int:id>', responses.delete_reply, name='delete_reply'),
+    path('ques_fav/',responses.ques_fav,name='ques_fav'),
+    path('show_fav_ques/',responses.show_fav_ques,name='show_fav_ques'),
+]
