3f61e1fef8dfd33fcdeaf36b3b0e36ad0d03a56b
MidHunterX <midhunterx@gmail.com>
2024-03-23 09:29:02 +0530

    🔦 Moved Theme Mode into Body

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,27 +3,27 @@
 \*================*/
 
 const themeToggle = document.getElementById('themeToggle');
-const htmlTag = document.querySelector('html');
+const bodyTag = document.querySelector('body');
 const currentTheme = localStorage.getItem('theme') || 'light';
 
 if (currentTheme === 'dark') {
-    htmlTag.setAttribute('data-bs-theme', 'dark');
+    bodyTag.setAttribute('data-bs-theme', 'dark');
     themeToggle.classList.add('fa-sun');
 } else {
-    htmlTag.setAttribute('data-bs-theme', 'light');
+    bodyTag.setAttribute('data-bs-theme', 'light');
     themeToggle.classList.add('fa-moon');
 }
 
 themeToggle.addEventListener('click', function() {
-    const currentTheme = htmlTag.getAttribute('data-bs-theme');
+    const currentTheme = bodyTag.getAttribute('data-bs-theme');
 
     if (currentTheme === 'dark') {
-        htmlTag.setAttribute('data-bs-theme', 'light');
+        bodyTag.setAttribute('data-bs-theme', 'light');
         localStorage.setItem('theme', 'light');
         themeToggle.classList.remove('fa-sun');
         themeToggle.classList.add('fa-moon');
     } else {
-        htmlTag.setAttribute('data-bs-theme', 'dark');
+        bodyTag.setAttribute('data-bs-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         themeToggle.classList.remove('fa-moon');
         themeToggle.classList.add('fa-sun');
