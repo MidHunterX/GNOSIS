2f0485e56eea5e168d1619fdad90a4f687d533a1
MidHunterX <midhunterx@gmail.com>
2024-04-07 12:24:13 +0530

    🥐 Updated: Crispy Registration Form Layout

--- gnosis_app/forms.py
+++ gnosis_app/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Question , Profile , Comment , Replies
 from django.contrib.auth.models import User
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Field, Layout, Row, Column, Submit, Reset
+from crispy_forms.bootstrap import PrependedText
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
@@ -17,7 +21,38 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password', 'confirm_password', 'DOB', 'photo']
-        labels = {'DOB': 'Date of Birth' }
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'password': 'Password',
+            'confirm_password': 'Confirm Password',
+            'DOB': 'Date of Birth',
+            'photo': 'Profile Picture'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            PrependedText('username', '@', placeholder='user_name'),
+            Row(
+                Column('password', css_class='form-group col'),
+                Column('confirm_password', css_class='form-group col'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('first_name', css_class='form-group col'),
+                Column('last_name', css_class='form-group col'),
+                css_class='form-row'
+            ),
+            'email',
+            'DOB',
+            'photo',
+            Submit('submit', 'Register', css_class='btn btn-primary'),
+            Reset('reset', 'Reset', css_class='btn btn-secondary'),
+        )
 
     def clean_confirm_password(self):
         password = self.cleaned_data['password']
